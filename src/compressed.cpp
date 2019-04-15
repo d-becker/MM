fbefccd51ec107ad7d5d37a9bed44e8f53403ebe
Daniel Becker <daniel.93.becker@gmail.com>
2019-04-15 22:03:58 +0200

    Only reference C version with compact structure.

--- src/compressed.cpp
+++ src/compressed.cpp
@@ -86,89 +86,89 @@ extern void compact_matrix_cell_centric(unsigned int sizex, unsigned int sizey, 
 
 	// Cell-centric algorithms
 	// Computational loop 1 - average density in cell
-  double t1 = omp_get_wtime();
-  auto INC = [] (double left, double right) {
-    return left + right;
-  };
-  IndexGenerator<2> index_generator({0, 0}, {sizex, sizey});
-  Computation<2> computation(data, index_generator);
-  computation.compute([] (double rho, double Vf, double V, ReduceProxy<double> rho_ave) {
-      rho_ave << rho*Vf/V;
-      },
-      IN<CellMatData<2>>(rho),
-      IN<CellMatData<2>>(Vf),
-      IN<CellData<2>>(V),
-      REDUCE<CellData<2>>(INC, rho_ave));
+  // double t1 = omp_get_wtime();
+  // auto INC = [] (double left, double right) {
+  //   return left + right;
+  // };
+  // IndexGenerator<2> index_generator({0, 0}, {sizex, sizey});
+  // Computation<2> computation(data, index_generator);
+  // computation.compute([] (double rho, double Vf, double V, ReduceProxy<double> rho_ave) {
+  //     rho_ave << rho*Vf/V;
+  //     },
+  //     IN<CellMatData<2>>(rho),
+  //     IN<CellMatData<2>>(Vf),
+  //     IN<CellData<2>>(V),
+  //     REDUCE<CellData<2>>(INC, rho_ave));
 
-  printf("Compact matrix, cell centric, alg 1: %g sec\n", omp_get_wtime()-t1);
+  // printf("Compact matrix, cell centric, alg 1: %g sec\n", omp_get_wtime()-t1);
 
   //C version
   compact_alg_1(sizex, sizey, rho.get_raw(), rho.get_raw_list(), V.get_raw(), Vf.get_raw(), Vf.get_raw_list(), rho_ave.get_raw(), data.structure);
 
 	// Computational loop 2 - Pressure for each cell and each material
-  t1 = omp_get_wtime();
-  Computation<2> computation3(data, index_generator);
-  computation3.compute([](double &p, double n, double rho, double t, double Vf) {
-      if (Vf > 0) p = (n * rho * t) / Vf;
-      else p = 0;
-      },
-      OUT<CellMatData<2>>(p),
-      IN<MatData<2>>(n),
-      IN<CellMatData<2>>(rho),
-      IN<CellMatData<2>>(t),
-      IN<CellMatData<2>>(Vf));
-  printf("Compact matrix, cell centric, alg 2: %g sec\n", omp_get_wtime()-t1);
+  // t1 = omp_get_wtime();
+  // Computation<2> computation3(data, index_generator);
+  // computation3.compute([](double &p, double n, double rho, double t, double Vf) {
+  //     if (Vf > 0) p = (n * rho * t) / Vf;
+  //     else p = 0;
+  //     },
+  //     OUT<CellMatData<2>>(p),
+  //     IN<MatData<2>>(n),
+  //     IN<CellMatData<2>>(rho),
+  //     IN<CellMatData<2>>(t),
+  //     IN<CellMatData<2>>(Vf));
+  // printf("Compact matrix, cell centric, alg 2: %g sec\n", omp_get_wtime()-t1);
   //C version
   compact_alg_2(sizex, sizey, p.get_raw(), p.get_raw_list(), n.get_raw(), rho.get_raw(), rho.get_raw_list(), t.get_raw(), t.get_raw_list(), Vf.get_raw(), Vf.get_raw_list(), data.structure);
 
 	// Computational loop 3 - Average density of each material over neighborhood of each cell
-  t1 = omp_get_wtime();
-  Stencil<2> s9pt({{1,1},  {1,0},  {1,-1},
-       {0,1},  {0,0},  {0,-1},
-       {-1,1}, {-1,0}, {-1,-1}});
-  IndexGenerator<2> index_generator2({1, 1}, {sizex-1, sizey-1});
-  Computation<2> computation2(data, index_generator2);
-  computation2.compute([](NeighProxy<CellData<2>> x, NeighProxy<CellData<2>> y,
-                          NeighProxy<CellMatData<2>> Vf, NeighProxy<CellMatData<2>> rho,
-                          double &rho_out) {
+  // t1 = omp_get_wtime();
+  // Stencil<2> s9pt({{1,1},  {1,0},  {1,-1},
+  //      {0,1},  {0,0},  {0,-1},
+  //      {-1,1}, {-1,0}, {-1,-1}});
+  // IndexGenerator<2> index_generator2({1, 1}, {sizex-1, sizey-1});
+  // Computation<2> computation2(data, index_generator2);
+  // computation2.compute([](NeighProxy<CellData<2>> x, NeighProxy<CellData<2>> y,
+  //                         NeighProxy<CellMatData<2>> Vf, NeighProxy<CellMatData<2>> rho,
+  //                         double &rho_out) {
 
 
-      double rho_sum = 0.0;
-      int Nn = 0;
-      if (Vf[{0,0}] > 0.0) {
-        double xo = x[{0,0}];
-        double yo = y[{0,0}];
-        for (int nj = -1; nj <= 1; nj++) {
-          for (int ni = -1; ni <= 1; ni++) {
-            NeighProxy<CellMatData<2>>::Token token = Vf.get_cell_mat_token({ni, nj});
+  //     double rho_sum = 0.0;
+  //     int Nn = 0;
+  //     if (Vf[{0,0}] > 0.0) {
+  //       double xo = x[{0,0}];
+  //       double yo = y[{0,0}];
+  //       for (int nj = -1; nj <= 1; nj++) {
+  //         for (int ni = -1; ni <= 1; ni++) {
+  //           NeighProxy<CellMatData<2>>::Token token = Vf.get_cell_mat_token({ni, nj});
 
-            // if (Vf.has_neigh({ni,nj})) {
-            if (token.is_valid()) {
-              double dsqr = 0.0;
+  //           // if (Vf.has_neigh({ni,nj})) {
+  //           if (token.is_valid()) {
+  //             double dsqr = 0.0;
 
-              // i: inner
-              double xi = x[{ni,nj}];
-              double yi = y[{ni,nj}];
+  //             // i: inner
+  //             double xi = x[{ni,nj}];
+  //             double yi = y[{ni,nj}];
 
-              dsqr += (xo - xi) * (xo - xi);
-              dsqr += (yo - yi) * (yo - yi);
+  //             dsqr += (xo - xi) * (xo - xi);
+  //             dsqr += (yo - yi) * (yo - yi);
 
-              // rho_sum += rho[{ni,nj}] / dsqr;
-              rho_sum += rho.get_with_token(token) / dsqr;
-              Nn += 1;
-            }
-          }
-        }
-        rho_out = rho_sum / Nn;
-      } else
-        rho_out = 0.0;
-    },
-    NEIGH<CellData<2>>(x, s9pt),
-    NEIGH<CellData<2>>(y, s9pt),
-    NEIGH<CellMatData<2>>(Vf, s9pt),
-    NEIGH<CellMatData<2>>(rho, s9pt),
-    OUT<CellMatData<2>>(rho_mat_ave));
-  printf("Compact matrix, cell centric, alg 3: %g sec\n", omp_get_wtime()-t1);
+  //             // rho_sum += rho[{ni,nj}] / dsqr;
+  //             rho_sum += rho.get_with_token(token) / dsqr;
+  //             Nn += 1;
+  //           }
+  //         }
+  //       }
+  //       rho_out = rho_sum / Nn;
+  //     } else
+  //       rho_out = 0.0;
+  //   },
+  //   NEIGH<CellData<2>>(x, s9pt),
+  //   NEIGH<CellData<2>>(y, s9pt),
+  //   NEIGH<CellMatData<2>>(Vf, s9pt),
+  //   NEIGH<CellMatData<2>>(rho, s9pt),
+  //   OUT<CellMatData<2>>(rho_mat_ave));
+  // printf("Compact matrix, cell centric, alg 3: %g sec\n", omp_get_wtime()-t1);
 
   //C version
   compact_alg_3(sizex, sizey, rho_mat_ave.get_raw(), rho_mat_ave.get_raw_list(), x.get_raw(), y.get_raw(), rho.get_raw(), rho.get_raw_list(), data.structure);
